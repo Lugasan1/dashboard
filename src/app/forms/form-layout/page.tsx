961975d17af5bafcc88b3fd442c388aead83f055
Vinicius Gasparello <vinygasparello@gmail.com>
2024-05-29 21:19:51 -0300

    feat: correct checkout page

--- src/app/forms/form-layout/page.tsx
+++ src/app/forms/form-layout/page.tsx
@@ -8,6 +8,8 @@ import {
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
+import { Contact, CreditCard } from "lucide-react";
+
 
 const FormLayout = () => {
   const searchParams = useSearchParams();
@@ -58,51 +60,29 @@ const FormLayout = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-9 bg-blue-100 p-8 sm:grid-cols-2">
-        <div className="flex flex-col gap-9">
-          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
-              <h3 className="font-medium text-black dark:text-white">
-                Address
-              </h3>
-            </div>
-            <form action="#">
-              <div className="p-6.5">
-                <AddressElement options={{ mode: "shipping" }} />
-              </div>
-            </form>
-          </div>
-          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
-              <h3 className="font-medium text-black dark:text-white">
-                Payment Method
-              </h3>
-            </div>
-            <div className="p-5">
-              <PaymentElement />
-            </div>
-          </div>
-        </div>
-        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-          <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
-            <h3 className="font-medium text-black dark:text-white">
-              Order overview
-            </h3>
-          </div>
+    <div className="w-full bg-slate-100">
+      <div className="flex flex-col gap-9 p-8 w-full justify-center items-center">
+      <div className="rounded-sm border border-stroke w-1/2 bg-white shadow-default">
           <div className="flex w-full flex-row p-4">
-            <div className="flex h-20 w-20 flex-row border border-stroke">
-              <Image src={ImageProduct} width={80} height={80} alt="" />
+            <div className="flex h-20 w-20 flex-row">
+              <Image className="rounded-full" src={ImageProduct} width={80} height={80} alt="" />
             </div>
             <div className="flex flex-col">
-              <div className="ml-5 w-full">
+              <div className=" flex flex-col ml-5 w-full">
                 <label
                   htmlFor=""
-                  className="text-md font-medium text-slate-800"
+                  className="text-lg font-medium text-slate-800"
+                >
+                 VOCÊ ESTÁ ADQUIRINDO
+                </label>
+                <label
+                  htmlFor=""
+                  className="text-lg font-black text-slate-800"
                 >
                   {productName ? productName.toString() : ""}
                 </label>
               </div>
-              <div className="ml-5 mt-5 flex w-full flex-row items-center gap-3">
+              <div className="ml-5 flex w-full flex-row items-center gap-3">
                 <label htmlFor="" className="text-md font-bold text-slate-800">
                   X{" "}
                   {price
@@ -114,27 +94,63 @@ const FormLayout = () => {
               </div>
             </div>
           </div>
+          
+      
+        </div>
+        <div className="flex flex-col w-1/2 gap-9">
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
+              <div className="flex flex-row items-center gap-4">
+                <span className="border-2 border-blue-600 text-blue-600 font-bold px-3 py-1 rounded-full">2</span>
+                <Contact className="text-blue-600 text-lg" />
+                <h3 className="font-bold text-lg text-blue-600 ">
+                ENDEREÇO
+              </h3>
+              </div>
+            </div>
+            <form action="#">
+              <div className="p-6.5">
+                <AddressElement options={{ mode: "shipping" }} />
+              </div>
+            </form>
+          </div>
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
+            <div className="flex flex-row items-center gap-4">
+                <span className="border-2 border-blue-600 text-blue-600 font-bold px-3 py-1 rounded-full">3</span>
+                <CreditCard className="text-blue-600 text-lg" />
+                <h3 className="font-bold text-lg text-blue-600 ">
+                FORMA DE PAGAMENTO
+              </h3>
+              </div>
+            </div>
+            <div className="p-5">
+              <PaymentElement />
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2">
           <div className="mx-6 border-t border-stroke py-4 dark:border-strokedark">
             <div className="mb-6 flex flex-row justify-between">
-              <label className="font-medium text-black dark:text-white">
+              <label className="font-medium text-lg text-black">
                 Subtotal
               </label>
-              <label className="font-bold text-black dark:text-white">
+              <label className="font-bold text-xl text-black">
                 {`R$ ${(total / 100).toLocaleString("pt-BR", {
                   minimumFractionDigits: 2,
                 })}`}
               </label>
             </div>
             <div className="mb-6 flex flex-row justify-between">
-            <label className="font-medium text-black dark:text-white">
-            Quantity
+            <label className="font-medium text-lg text-black">
+            Quantidade
               </label>
-              <label className="font-bold text-black dark:text-white">
+              <label className="font-bold text-lg text-black">
               {quantity}
               </label>
             </div>
           </div>
-          <div className="mx-6 border-t border-stroke py-4 dark:border-strokedark">
+          <div className="mx-6 py-4">
             <div className="mb-6 flex flex-row justify-between">
               <label className="font-medium text-black dark:text-white">
                 Total
@@ -158,7 +174,9 @@ const FormLayout = () => {
             {message && <div id="payment-message">{message}</div>}
           </div>
         </div>
+        
       </div>
+    </div>
     </>
   );
 };
